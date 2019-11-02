a02cd0ce1daee146fac556850c005cb0f94c3e44
senykvolodya <52894135+senykvolodya@users.noreply.github.com>
2019-11-02 18:50:51 +0200

    js added

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function initMap() {
+  var sedona = {lat: 34.8812803, lng: -111.8009852};
+  var map = new google.maps.Map(document.querySelector('#map'), {
+    center: {lat: 34.75, lng: -111.8009852},
+    zoom: 9,
+    disableDefaultUI: true
+  });
+  var marker = new google.maps.Marker({
+    position: sedona,
+    map: map
+  });
+}
+
+var modal = document.querySelector('.modal');
+var modalButton = document.querySelector('.button-modal');
+
+modalButton.addEventListener('click', function () {
+  modal.classList.toggle('modal-show');
+});
+
+document.addEventListener('keydown', function (evt) {
+  if (evt.key === 'Escape') {
+    modal.classList.remove('modal-show');
+  }
+});
