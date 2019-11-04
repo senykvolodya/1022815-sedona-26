21a7115cc4144949f2463e66088dc3db782078da
senykvolodya <52894135+senykvolodya@users.noreply.github.com>
2019-11-04 17:31:20 +0200

    bugs fixed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function (callback, thisArg) {
+    thisArg = thisArg || window;
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
+  };
+}
+
+
 function initMap() {
   var sedona = {lat: 34.8812803, lng: -111.8009852};
-  var map = new google.maps.Map(document.querySelector('#map'), {
+  var map = new google.maps.Map(document.querySelector('.map-api'), {
     center: {lat: 34.75, lng: -111.8009852},
     zoom: 9,
     disableDefaultUI: true
@@ -10,6 +20,7 @@ function initMap() {
     map: map
   });
 }
+
 
 var modal = document.querySelector('.modal');
 var modalButton = document.querySelector('.button-modal');
@@ -25,4 +36,20 @@ document.addEventListener('keydown', function (evt) {
   if (evt.key === 'Escape') {
     modal.classList.remove('modal-show');
   }
+});
+
+
+var inputs = modal.querySelectorAll('input');
+var buttonSearch = modal.querySelector('.button-search');
+var searchForm = modal.querySelector('.search-form');
+
+buttonSearch.addEventListener('click', function (evt) {
+  inputs.forEach(function (item) {
+    if(item.value.length === 0) {
+      evt.preventDefault();
+      searchForm.classList.remove('modal-error');
+      searchForm.offsetWidth = searchForm.offsetWidth;
+      searchForm.classList.add('modal-error');
+    }
+  });
 });
